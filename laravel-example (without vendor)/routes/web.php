c635b9e0e1c5fcca0986f25ac04c3963e5a56ca7
Nikita <nikitaBorzhak@gmail.com>
2025-12-16 02:23:00 +0200

    asgag

--- laravel-example (without vendor)/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', fn() => redirect()->route('products.index'));
-Route::resource('products', ProductController::class);
-
